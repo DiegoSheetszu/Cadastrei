334e88a900bd7793ee38620151a428fbd190ac15
DiegoSheetszu <diego@sheetszu.com>
2026-02-14 14:19:36 -0300

    Adicionado Query de Afastamentos e vinculado um dataframe na main

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
+import pandas as pd
 from config.engine import ativar_engine
-from Consultas_dbo.afastamentos import RepositorioAfastamentos
+from Consultas_dbo.afastamentos.afastamentos import RepositorioAfastamentos
+
 
 def main():
-    # 1) Escolhe a database de origem
     database = "SOFTRAN_COMTRASIL"
-
-    # 2) Cria engine
     engine = ativar_engine(database)
 
-    # 3) Cria o repositório
     repo = RepositorioAfastamentos(engine)
+    afastamentos = repo.buscar_dados_afastamentos(limit=10)
+    df = pd.DataFrame(afastamentos)
 
-    # 4) Chama a query
-    since = "2026-02-01 00:00:00"
-    drivers = repo.buscar_dados_afastamentos(since=since, limit=10)
+    print(f"Total retornado: {len(df)}")
+    print(df.head())
 
-    # 5) Usa o resultado
-    print(f"Total retornado: {len(drivers)}")
-    for d in drivers:
-        print(d)
 
 if __name__ == "__main__":
     main()
-
-
-
